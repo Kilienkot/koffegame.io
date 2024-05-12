94d51cc2724be7d83d7f9a5a56367ed2445ef87d
Kilienkot <114224515+Kilienkot@users.noreply.github.com>
2024-05-12 03:33:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.querySelectorAll('.menu__link').forEach((link) => {
+          let id = link.getAttribute('href').replace('#', '');
+          if (id === entry.target.id) {
+            link.classList.add('menu__link_active');
+          } else {
+            link.classList.remove('menu__link_active');
+          }
+        });
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+  
+  document.querySelectorAll('.section').forEach(section => { observer.observe(section)} );
